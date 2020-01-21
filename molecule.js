81070f6dd509715407ca4f442fded36519915cd1
John Ramos <N00172468@iadt.ie>
2020-01-21 10:27:16 +0000

    Initial Commit

--- molecule.js
+++ molecule.js
@@ -8,7 +8,7 @@ class Molecule {
     this.velocity = createVector(random(-15, 20), random(-15, 20));
     this.moleculeId = _moleculeId; // Tracking I.D per Object (Starting at Object I.D: 0)
     this.molFill = false; // Object Fill colour = False by default
-    this.angle = 41.5; // Starting angle of aesthetic design within Object
+    // this.angle = 41.5; // Starting angle of aesthetic design within Object
     this.checks; // Attribute for Brute-force Checking function
     this.checkIntersection; // Attribute for Intersection Checking function
   }
@@ -24,12 +24,12 @@ class Molecule {
     push();
     stroke(40, 126, 235);
     strokeWeight(2);
-    let c = tan(this.angle); // Allows angle of aesthetics to be animated (in this case, rotating)
+    // let c = tan(this.angle); // Allows angle of aesthetics to be animated (in this case, rotating)
     translate(this.position.x, this.position.y); // Displace 0,0 coordinates
-    rotate(c); // Rotate aesthetics
-    this.angle++; // Animate aesthetics
-    ellipse(0, 0, this.radius * 1.8, this.radius * 0.2); // Aesthetics (i.e. Oval-1)
-    ellipse(0, 0, this.radius * 0.2, this.radius * 1.8); // Aesthetics (i.e. Oval-2)
+    // rotate(c); // Rotate aesthetics
+    // this.angle++; // Animate aesthetics
+    // ellipse(0, 0, this.radius * 0.2, this.radius * 1.8); // Aesthetics (i.e. Oval-2)
+    // ellipse(0, 0, this.radius * 1.8, this.radius * 0.2); // Aesthetics (i.e. Oval-1)
     ellipse(0, 0, this.radius * 2, this.radius * 2); // Main Object (i.e. Molecule)
     pop();
 
@@ -53,15 +53,15 @@ class Molecule {
   }
 
   // **BRUTE-CHECKS** - Brute-force Checking:
-  bruteChecks() {
-    for (let i = 0; i < molecules.length; i++) {
-      for (let j = 0; j < molecules.length; j++) {
-        if (p5.Vector.sub(molecules[i].position, molecules[j].position).mag() < radius) {
-          checks++;
-        }
-      }
-    }
-  }
+  // bruteChecks() {
+  //   for (let i = 0; i < molecules.length; i++) {
+  //     for (let j = 0; j < molecules.length; j++) {
+  //       if (p5.Vector.sub(molecules[i].position, molecules[j].position).mag() < radius) {
+  //         checks++;
+  //       }
+  //     }
+  //   }
+  // }
 
   // **CHECK-INTERSECTIONS** - No. of Intersections:
   checkIntersections() {
@@ -77,61 +77,61 @@ class Molecule {
   }
 
   // **SPLIT-INTO-GRIDS** - Check Molecules if in Specific Grid (Reference: xCell = i / yCell = j):
-  splitIntoGrids() {
-    // Refresh to prevent stacking
-    this.molFill = 0;
+  // splitIntoGrids() {
+  //   // Refresh to prevent stacking
+  //   this.molFill = 0;
 
-    molecules.forEach(molecule => {
-      tempArray = [];
+  //   molecules.forEach(molecule => {
+  //     tempArray = [];
 
-      for (let xCell = 0; xCell < numRows; xCell++) {
-        for (let yCell = 0; yCell < numCols; yCell++) {
-          let rowHeight = height / numCols;
+  //     for (let xCell = 0; xCell < numRows; xCell++) {
+  //       for (let yCell = 0; yCell < numCols; yCell++) {
+  //         let rowHeight = height / numCols;
 
-          if (
-            molecule.position.x < rowWidth * (xCell + 1) &&
-            molecule.position.x > rowWidth * xCell &&
-            molecule.position.y < rowHeight * (yCell + 1) &&
-            molecule.position.y > rowHeight * yCell
-          ) {
-            tempArray.push(molecule.moleculeId);
+  //         if (
+  //           molecule.position.x < rowWidth * (xCell + 1) &&
+  //           molecule.position.x > rowWidth * xCell &&
+  //           molecule.position.y < rowHeight * (yCell + 1) &&
+  //           molecule.position.y > rowHeight * yCell
+  //         ) {
+  //           tempArray.push(molecule.moleculeId);
 
-            // ---UN-COMMENT BELOW FOR TESTING (WARNING: FPS WILL DROP A GOOD AMOUNT!)---
-            // console.table("---MOL ID---: " + this.moleculeId);
-            // console.table("xCell: " + xCell + "     yCell: " + yCell);
-            // console.table("X Pos: " + round(molecule.position.x) + "   Y Pos: " + round(molecule.position.y));
-          }
-        }
-      }
-    });
-    return tempArray;
-  }
+  //           // ---UN-COMMENT BELOW FOR TESTING (WARNING: FPS WILL DROP A GOOD AMOUNT!)---
+  //           // console.table("---MOL ID---: " + this.moleculeId);
+  //           // console.table("xCell: " + xCell + "     yCell: " + yCell);
+  //           // console.table("X Pos: " + round(molecule.position.x) + "   Y Pos: " + round(molecule.position.y));
+  //         }
+  //       }
+  //     }
+  //   });
+  //   return tempArray;
+  // }
 
   // **DRAW-GRID** - Display Grid:
-  drawGrid() {
-    let colWidth = width / numCols;
-    let rowHeight = height / numRows;
+  // drawGrid() {
+  //   let colWidth = width / numCols;
+  //   let rowHeight = height / numRows;
 
-    for (let y = 0; y < numRows; y++) {
-      for (let x = 0; x < numCols; x++) {
-        noFill();
-        stroke(60, 60, 60);
-        rect(x * colWidth, y * rowHeight, colWidth, rowHeight);
-      }
-    }
+  //   for (let y = 0; y < numRows; y++) {
+  //     for (let x = 0; x < numCols; x++) {
+  //       noFill();
+  //       stroke(60, 60, 60);
+  //       rect(x * colWidth, y * rowHeight, colWidth, rowHeight);
+  //     }
+  //   }
 
-    // Mol Key Array:
-    tempArray.push[molecules];
-    molPerGrid.push[tempArray];
-  }
+  //   // Mol Key Array:
+  //   tempArray.push[molecules];
+  //   molPerGrid.push[tempArray];
+  // }
 
   // **STATISTICS** - Display Statistics (Top Left Corner):
-  statistics() {
-    fill(47, 226, 255);
-    textSize(11);
-    text("FPS: " + frameRate().toFixed(0), 5, 14);
-    text("No. of Mols: " + numOfMolecules, 5, 25);
-    text("Intersections: " + checkIntersection, 5, 36);
-    text("Brute Checks: " + checks, 5, 47);
-  }
+  // statistics() {
+  //   fill(47, 226, 255);
+  //   textSize(11);
+  //   text("FPS: " + frameRate().toFixed(0), 5, 14);
+  //   text("No. of Mols: " + numOfMolecules, 5, 25);
+  //   text("Intersections: " + checkIntersection, 5, 36);
+  //   text("Brute Checks: " + checks, 5, 47);
+  // }
 }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 // Radius:
-var minRadius = 20;
+var minRadius = 25;
 var maxRadius = 40;
 let radius = minRadius + maxRadius;
 
 // Molecules:
 let molecules = [];
-var numOfMolecules = 20; // Default for GUI
+var numOfMolecules = 30; // Default for GUI
 
 // Rows and Columns:
-var numRows = 6; // Default for GUI
-var numCols = 12; // Default for GUI
+var numRows = 4; // Default for GUI
+var numCols = 8; // Default for GUI
 let rowWidth;
 let rowHeight;
 
@@ -42,7 +42,7 @@ function setup() {
   // Generate GUI:
   let gui_col = new dat.GUI(); // Calling GUI features from dat.gui.min.js
   gui_col
-    .add(this, "numOfMolecules", 1, 100) // No. of Molecules = Min: 1, Max: 100
+    .add(this, "numOfMolecules", 1, 1000) // No. of Molecules = Min: 1, Max: 1000
     .step(1) // Convert float values into int values
     .onChange(function() {
       guiMols();
@@ -73,13 +73,13 @@ function draw() {
   molecules.forEach(molecule => {
     // If loops for GUI checkboxes
     this.renderMols ? molecule.render() : this.renderMols = false;
-    this. moveMols ? molecule.step() : this.moveMols = false;
+    this.moveMols ? molecule.step() : this.moveMols = false;
     this.checkEdge ? molecule.checkEdges() : this.checkEdge = false;
-    this.bruteForceChecks ? molecule.bruteChecks() : this.bruteForceChecks = false; 
-    this.intersectionChecks ? molecule.checkIntersections() : this.intersectionChecks = false;
-    this.grid ? molecule.drawGrid() : this.grid = false; 
-    this.molsPerCell ? molecule.splitIntoGrids() : this.molsPerCell = false;
-    this.showStats ? molecule.statistics() : this.showStats = false;
+    // this.bruteForceChecks ? molecule.bruteChecks() : this.bruteForceChecks = false; 
+    // this.intersectionChecks ? molecule.checkIntersections() : this.intersectionChecks = false;
+    // this.grid ? molecule.drawGrid() : this.grid = false; 
+    // this.molsPerCell ? molecule.splitIntoGrids() : this.molsPerCell = false;
+    // this.showStats ? molecule.statistics() : this.showStats = false;
   });
 
   // console.timeEnd("Time Taken");
@@ -93,4 +93,87 @@ function guiMols() {
   for (let i = 0; i < numOfMolecules; i++) {
     molecules.push(new Molecule(i));
   }
+}
+
+// **BRUTE-CHECKS** - Brute-force Checking:
+function bruteChecks() {
+  for (let i = 0; i < molecules.length; i++) {
+    for (let j = 0; j < molecules.length; j++) {
+      if (p5.Vector.sub(molecules[i].position, molecules[j].position).mag() < radius) {
+        checks++;
+      }
+    }
+  }
+}
+
+// **CHECK-INTERSECTIONS** - No. of Intersections:
+function checkIntersections() {
+  for (let i = 0; i < molecules.length; i++) {
+    for (let j = i + 1; j < molecules.length; j++) {
+      if (p5.Vector.sub(molecules[i].position, molecules[j].position).mag() < radius) {
+        checkIntersection++;
+        molecules[i].molFill = true; // When intersect, fill in Object i
+        molecules[j].molFill = true; // When intersect, fill in Object j
+      }
+    }
+  }
+}
+
+// **SPLIT-INTO-GRIDS** - Check Molecules if in Specific Grid (Reference: xCell = i / yCell = j):
+function splitIntoGrids() {
+  // Refresh to prevent stacking
+  this.molFill = 0;
+
+  molecules.forEach(molecule => {
+    tempArray = [];
+
+    for (let xCell = 0; xCell < numRows; xCell++) {
+      for (let yCell = 0; yCell < numCols; yCell++) {
+        let rowHeight = height / numCols;
+
+        if (
+          molecule.position.x < rowWidth * (xCell + 1) &&
+          molecule.position.x > rowWidth * xCell &&
+          molecule.position.y < rowHeight * (yCell + 1) &&
+          molecule.position.y > rowHeight * yCell
+        ) {
+          tempArray.push(molecule.moleculeId);
+
+          // ---UN-COMMENT BELOW FOR TESTING (WARNING: FPS WILL DROP A GOOD AMOUNT!)---
+          // console.table("---MOL ID---: " + this.moleculeId);
+          // console.table("xCell: " + xCell + "     yCell: " + yCell);
+          // console.table("X Pos: " + round(molecule.position.x) + "   Y Pos: " + round(molecule.position.y));
+        }
+      }
+    }
+  });
+  return tempArray;
+}
+
+// **DRAW-GRID** - Display Grid:
+function drawGrid() {
+  let colWidth = width / numCols;
+  let rowHeight = height / numRows;
+
+  for (let y = 0; y < numRows; y++) {
+    for (let x = 0; x < numCols; x++) {
+      noFill();
+      stroke(60, 60, 60);
+      rect(x * colWidth, y * rowHeight, colWidth, rowHeight);
+    }
+  }
+
+  // Mol Key Array:
+  tempArray.push[molecules];
+  molPerGrid.push[tempArray];
+}
+
+// **STATISTICS** - Display Statistics (Top Left Corner):
+function statistics() {
+  fill(47, 226, 255);
+  textSize(11);
+  text("FPS: " + frameRate().toFixed(0), 5, 14);
+  text("No. of Mols: " + numOfMolecules, 5, 25);
+  text("Intersections: " + checkIntersection, 5, 36);
+  text("Brute Checks: " + checks, 5, 47);
 }
